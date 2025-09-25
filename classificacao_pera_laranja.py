ab5f80cf878465236101f36ebafc86cb7495b653
Antonino Marques Jares <144476542+Antonino-Marques-Jares@users.noreply.github.com>
2025-09-25 13:08:29 -0300

    Add files via upload

---
+++ classificacao_pera_laranja.py
@@ -0,0 +1,28 @@
+
+# treinamento supervisionado - classificação
+
+from sklearn import tree
+
+lisa=1
+irregular=0
+pera=1
+laranja=0
+
+pomar = [[120,lisa],[140, lisa], [180, irregular], [200, irregular] ]
+
+resultado = [pera, pera, laranja, laranja]
+
+# gerar uma árvore de decisão
+clf = tree.DecisionTreeClassifier()
+# classificador
+clf = clf.fit(pomar, resultado)
+peso = 220
+# 1 para lisa e O para irregular
+superficie = 0
+
+resultadousu = clf.predict([[peso, superficie]])
+
+if resultadousu == 1:
+    print('Pera')
+else:
+    print('Laranja')
